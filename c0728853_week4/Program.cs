e403ba9581c46ba576f527a59830ef59ec02036e
visualstudio <visualstudio@230-PC34>
2019-01-31 14:43:09 -0500

    coding

--- c0728853_week4/Program.cs
+++ c0728853_week4/Program.cs
@@ -11,6 +11,49 @@ namespace c0728853_week4
         static void Main(string[] args)
         {
         }
+        class Village
+        {
+            public static int numberOfVillages = 0;
+            public Village nextVillage;
+            public Village previousVillage;
+            public string VillageName;
+            public bool isAstrildeHere = false;
+
+            public Village()
+            {
+                Village.numberOfVillages++;
+            }
+        }
+        class Countryside
+        {
+            public Village Maple;
+            public Village Toronto;
+            public Village Ajax;
+
+            public void MapInitializer()
+            {
+                Maple = new Village();
+                Maple.VillageName = "Maple";
+                Maple.previousVillage = null;
+                Maple.nextVillage = Toronto;
+                Toronto = new Village();
+                Toronto.previousVillage = Maple;
+
+
+                Toronto.VillageName = "Toronto";
+                Toronto.nextVillage = Ajax;
+                Ajax = new Village();
+                Ajax.VillageName = "Ajax";
+                Ajax.VillageName = null;
+                Ajax.previousVillage = Toronto;
+            }
+        }
+        public void LookForAstrilde()
+        {
+            //gg
+        }
     }
 }
+
+
 
